c8f4b51f558da387221e9828ba90209e4899ad16
ragavanrocky <52520783+ragavanrocky@users.noreply.github.com>
2019-07-11 17:29:38 +0530

    Update three.c

--- three.c
+++ three.c
@@ -7,6 +7,6 @@ void main()
     printf("Vowel");
   else if(c=='b'||c=='c'||c=='d'||c=='f'||c=='g'||c=='h'||c=='j'||c=='k'||c=='l'||c=='m'||c=='n'||c=='p'||c=='q'||c=='r'||c=='s'||c=='t'||c=='v'||c=='w'||c=='x'||c=='y||c=='z')
     printf("Consonant");
-  else
+ else
     printf("invalid");
 }
